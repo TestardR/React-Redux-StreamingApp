077f047fc6035b87f5a8be9a4a798f379fc8656f
unknown <rmtestard@gmail.com>
2019-04-02 19:32:29 +0200

    bulk actions creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,38 @@
 import streams from '../apis/streams';
-import { CREATE_STREAM } from './types';
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM
+} from './types';
 
 export const createStream = formValues => async dispatch => {
   const response = await streams.post('/streams', formValues);
   //   console.log(response);
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues);
+
+  dispatch({ type: editStream, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
