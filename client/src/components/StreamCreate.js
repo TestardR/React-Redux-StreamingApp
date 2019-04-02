9b11d7ae2c0cbac46d164d45b40d2733ae074640
unknown <rmtestard@gmail.com>
2019-04-02 17:06:50 +0200

    wiring props in reduxForm

--- client/src/components/StreamCreate.js
+++ client/src/components/StreamCreate.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>Create</div>;
-};
+class StreamCreate extends Component {
+  renderInput(formProps) {
+    // we could have destructured {input}
+    // console.log(formProps);
+    return <input {...formProps.input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    // console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
