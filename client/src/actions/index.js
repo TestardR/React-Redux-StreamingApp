cf3362a2c6322cea22887f53af9d36f49671e7bd
unknown <rmtestard@gmail.com>
2019-04-02 19:27:12 +0200

    bulk actions creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,9 @@
 import streams from '../apis/streams';
+import { CREATE_STREAM } from './types';
 
-export const createStream = formValues => async ispatch => {
-  streams.post('/streams', formValues);
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post('/streams', formValues);
+  //   console.log(response);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
